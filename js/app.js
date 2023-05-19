748ed5e8ec39e042e16ba0fae70081514cca970f
AmmarAhmed1322004@outlook.com <Ammar132004@gmail.com>
2023-05-19 12:23:56 +0200

    ED

--- js/app.js
+++ js/app.js
@@ -32,86 +32,115 @@ document.querySelectorAll("nav li").forEach((item) => {
 
 checkActive();
 
-// .........................
+// // .........................
 
-let En = document.getElementById("terms-checkbox-37");
+// let En = document.getElementById("terms-checkbox-37");
 
-let translatedText = document.getElementById("translatedText");
-let testAsfour = document.querySelector(".asfour");
-let testTrans = document.querySelectorAll(".testtran");
-var textToTranslate = "";
-for (let i of testTrans) {
-  textToTranslate += i.innerText + "-"; // يجب استبدال هذا النص بالنص المراد ترجمته
-}
-let engLetter = "";
+// let translatedText = document.getElementById("translatedText");
+// let testAsfour = document.querySelector(".asfour");
+// let testTrans = document.querySelectorAll(".testtran");
+// var textToTranslate = "";
+// for (let i of testTrans) {
+//   textToTranslate += i.innerText + "-"; // يجب استبدال هذا النص بالنص المراد ترجمته
+// }
+// let engLetter = "";
 
-testAsfour.addEventListener('change',function(){
-  function trns() {
-    var xhr = new XMLHttpRequest();
-    var langPair = "en-ar"; // يجب تحديد الزوج اللغوي المراد استخدامه، وهنا هو اللغة الإنجليزية إلى العربية
-    var url =
-      "https://translate.googleapis.com/translate_a/single?client=gtx&sl=" +
-      langPair.split("-")[0] +
-      "&tl=" +
-      langPair.split("-")[1] +
-      "&dt=t&q=" +
-      encodeURI(textToTranslate);
-    xhr.open("GET", url);
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        var response = JSON.parse(xhr.responseText);
-        for (let i = 0; i < testTrans.length; i++) {
-          testTrans[i].innerText = response[0][0][0].split("-")[i];
-        }
-      }
-    };
-    xhr.send();
-  }
-  if (testAsfour.value == '3') {
-    engLetter = textToTranslate;
-    trns();
-  } else {
-    for (let i = 0; i < testTrans.length; i++) {
-      console.log(engLetter)
-      testTrans[i].innerText = engLetter.split("-")[i];
-    }
-  }
+// testAsfour.addEventListener('change',function(){
+//   function trns() {
+//     var xhr = new XMLHttpRequest();
+//     var langPair = "en-ar"; // يجب تحديد الزوج اللغوي المراد استخدامه، وهنا هو اللغة الإنجليزية إلى العربية
+//     var url =
+//       "https://translate.googleapis.com/translate_a/single?client=gtx&sl=" +
+//       langPair.split("-")[0] +
+//       "&tl=" +
+//       langPair.split("-")[1] +
+//       "&dt=t&q=" +
+//       encodeURI(textToTranslate);
+//     xhr.open("GET", url);
+//     xhr.onreadystatechange = function () {
+//       if (xhr.readyState === 4 && xhr.status === 200) {
+//         var response = JSON.parse(xhr.responseText);
+//         for (let i = 0; i < testTrans.length; i++) {
+//           testTrans[i].innerText = response[0][0][0].split("-")[i];
+//         }
+//       }
+//     };
+//     xhr.send();
+//   }
+//   if (testAsfour.value == '3') {
+//     engLetter = textToTranslate;
+//     trns();
+//   } else {
+//     for (let i = 0; i < testTrans.length; i++) {
+//       console.log(engLetter)
+//       testTrans[i].innerText = engLetter.split("-")[i];
+//     }
+//   }
+// })
+
+// // let label = document.getElementById('label');
+
+// En.addEventListener("click", function () {
+
+
+//   En.classList.toggle("ward");
+
+//   if (En.classList.value.includes("ward")) {
+//     engLetter = textToTranslate;
+//     trns();
+//   } else {
+//     for (let i = 0; i < testTrans.length; i++) {
+//       testTrans[i].innerText = engLetter.split("-")[i];
+//     }
+//   }
+//   function trns() {
+//     var xhr = new XMLHttpRequest();
+//     var langPair = "en-ar"; // يجب تحديد الزوج اللغوي المراد استخدامه، وهنا هو اللغة الإنجليزية إلى العربية
+//     var url =
+//       "https://translate.googleapis.com/translate_a/single?client=gtx&sl=" +
+//       langPair.split("-")[0] +
+//       "&tl=" +
+//       langPair.split("-")[1] +
+//       "&dt=t&q=" +
+//       encodeURI(textToTranslate);
+//     xhr.open("GET", url);
+//     xhr.onreadystatechange = function () {
+//       if (xhr.readyState === 4 && xhr.status === 200) {
+//         var response = JSON.parse(xhr.responseText);
+//         for (let i = 0; i < testTrans.length; i++) {
+//           testTrans[i].innerText = response[0][0][0].split("-")[i];
+//         }
+//       }
+//     };
+//     xhr.send();
+//   }
+// });
+
+
+let asfour = document.getElementsByClassName('asfour');
+
+let ENG = document.getElementById('ENG');
+
+let ARB = document.getElementById('ARB');
+
+let ENGLANG = document.getElementById('ENGLANG');
+
+let ARBLANG = document.getElementById('ARBLANG');
+
+
+asfour[0].addEventListener('change',function(){
+
+  ARBLANG.style.display='block';
+
+  ENGLANG.style.display='none';
+
 })
 
-// let label = document.getElementById('label');
 
-En.addEventListener("click", function () {
+asfour[1].addEventListener('change',function(){
 
+  ARBLANG.style.display='none';
 
-  En.classList.toggle("ward");
+  ENGLANG.style.display='none';
 
-  if (En.classList.value.includes("ward")) {
-    engLetter = textToTranslate;
-    trns();
-  } else {
-    for (let i = 0; i < testTrans.length; i++) {
-      testTrans[i].innerText = engLetter.split("-")[i];
-    }
-  }
-  function trns() {
-    var xhr = new XMLHttpRequest();
-    var langPair = "en-ar"; // يجب تحديد الزوج اللغوي المراد استخدامه، وهنا هو اللغة الإنجليزية إلى العربية
-    var url =
-      "https://translate.googleapis.com/translate_a/single?client=gtx&sl=" +
-      langPair.split("-")[0] +
-      "&tl=" +
-      langPair.split("-")[1] +
-      "&dt=t&q=" +
-      encodeURI(textToTranslate);
-    xhr.open("GET", url);
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        var response = JSON.parse(xhr.responseText);
-        for (let i = 0; i < testTrans.length; i++) {
-          testTrans[i].innerText = response[0][0][0].split("-")[i];
-        }
-      }
-    };
-    xhr.send();
-  }
-});
+})
