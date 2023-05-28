84c678f09073d170720451dd87fc35902b8b0ab2
capYousef <elcaptain.yousef.official@gmail.com>
2023-05-28 10:28:47 +0300

    Activate Register New Account Form

--- js/auth.js
+++ js/auth.js
@@ -1,31 +1,27 @@
-(function () {
-  let signUpForm = document.getElementById("sign_up_form"),
-    signUpSubmit = document.getElementById("sign_up_submit");
+let signUpForm = document.getElementById("sign_up_form"),
+  signUpSubmit = document.getElementById("sign_up_submit");
 
-  signUpSubmit.addEventListener("click", function (event) {
-    event.preventDefault();
+signUpSubmit.addEventListener("click", function (event) {
+  event.preventDefault();
 
-    // Accessing the form fields
-    const firstName = signUpForm.elements["firstName"].value,
-      lastName = signUpForm.elements["lastName"].value,
-      email = signUpForm.elements["email"].value,
-      phone = signUpForm.elements["phone"].value,
-      password = signUpForm.elements["password"].value,
-      password_confirm = signUpForm.elements["password_confirm"].value,
-      id = signUpForm.elements["id"].value;
+  // Accessing the form fields
+  const firstName = signUpForm.elements["firstName"].value,
+    lastName = signUpForm.elements["lastName"].value,
+    email = signUpForm.elements["email"].value,
+    phone = signUpForm.elements["phone"].value,
+    password = signUpForm.elements["password"].value,
+    password_confirm = signUpForm.elements["password_confirm"].value,
+    id = signUpForm.elements["id"].value;
 
-    let data = {
-      name: firstName + " " + lastName,
-      email: email,
-      phone: phone,
-      password: password,
-      password_confirmation: password_confirm,
-      id_document: id,
-    };
-    postData(APIs.host + APIs.user.register, data, null, "POST").then(
-      (data) => {
-        console.log(data);
-      }
-    );
+  let data = {
+    name: firstName + " " + lastName,
+    email: email,
+    phone: phone,
+    password: password,
+    password_confirmation: password_confirm,
+    id_document: id,
+  };
+  postData(APIs.host + APIs.user.register, data, null, "POST").then((data) => {
+    console.log(data);
   });
-})();
+});
