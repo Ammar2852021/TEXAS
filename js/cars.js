fe4963c8dae7409d2d213b20c596298327e59067
AmmarAhmed1322004@outlook.com <Ammar132004@gmail.com>
2023-05-31 23:14:49 +0300

    ED

--- js/cars.js
+++ js/cars.js
@@ -139,3 +139,18 @@ exit.addEventListener('click',function()
 
 })
 
+
+
+
+// ..................... range
+
+let range = document.getElementById('range')
+console.log(range);
+
+var slider = document.getElementById("range");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
