bc00f65499f3835176055a7f26a3846b9de324ae
AmmarAhmed1322004@outlook.com <Ammar132004@gmail.com>
2023-06-03 03:35:35 +0300

    ed

--- js/home.js
+++ js/home.js
@@ -1,9 +1,16 @@
 let Address = document.getElementById('Address');
 
+let suggestions = document.getElementById('suggestions');
+let Arr = [4554,156456,1,45,564,45];
 
-Address.addEventListener('change',function()
+Arr.forEach(function(e){
+    suggestions.innerHTML+= `<option>${e}</option>`
+})
+
+
+Address.addEventListener('input',function(e)
 {
-    console.log(Address.value);
+    console.log(e.target.value);
 })
 
 
