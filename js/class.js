d82c54910a73506898e4e5386a6a92db702fd62a
AmmarAhmed1322004@outlook.com <Ammar132004@gmail.com>
2023-06-07 03:34:44 +0300

    ED

---
+++ js/class.js
@@ -0,0 +1,13 @@
+// let bordarclass = document.getElementById("bordar-class");
+// console.log(bordarclass);
+
+
+
+let bordarclass = document.getElementById("bordar-class");
+console.log(bordarclass);
+
+
+
+bordarclass.addEventListener("click", function () {
+  bordarclass.classList.toggle("class");
+});
