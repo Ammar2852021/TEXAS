1888a239a4b2b5153cdb3269855f7eef7e9f87d3
AmmarAhmed1322004@outlook.com <Ammar132004@gmail.com>
2023-06-07 03:39:09 +0300

    ED

--- js/class.js
+++ js/class.js
@@ -1,11 +1,8 @@
-// let bordarclass = document.getElementById("bordar-class");
-// console.log(bordarclass);
 
-
+// bordar color on click
 
 let bordarclass = document.getElementById("bordar-class");
 console.log(bordarclass);
-
 
 
 bordarclass.addEventListener("click", function () {
