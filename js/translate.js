d326a4c3563b32db19e27a31f876d95192c922fb
Ammar Ahmed <AmmarAhmed132004@outlook.com>
2023-06-21 04:25:21 +0300

    ED

--- js/translate.js
+++ js/translate.js
@@ -1,75 +1,83 @@
-let translate = document.getElementById("translate");
-let htext = document.querySelectorAll(".tr");
+// let translate = document.getElementById("translate");
+// let htext = document.querySelectorAll(".tr");
 
 
-let ARB = document.getElementById("ARB");
-let ENG = document.getElementById("ENG");
-let footer = document.getElementById('footer')
+// let ARB = document.getElementById("ARB");
+// let ENG = document.getElementById("ENG");
+// let footer = document.getElementById('footer')
 
 
-let ENGLANG2 = document.querySelectorAll ('.ENGLANG-tr');
-let ARBLANG2 = document.querySelectorAll ('.ARBLANG-tr');
+// let ENGLANG2 = document.querySelectorAll ('.ENGLANG-tr');
+// let ARBLANG2 = document.querySelectorAll ('.ARBLANG-tr');
 
-let Aboutus=document.getElementById('About-us');
+// let Aboutus=document.getElementById('About-us');
 
-console.log(ENGLANG2);
+// console.log(ENGLANG2);
 
 
-ARB.addEventListener("change", function () {
-  console.log("Ammar Ahmed mostaf");
-});
+// ARB.addEventListener("change", function () {
+//   console.log("Ammar Ahmed mostaf");
+// });
 
-let englishArray = [];
-htext.forEach((text) => {
-  englishArray.push(text.innerText);
-});
-function trans(arrOfLetters, fromLang, toLang) {
-  fetch(
-    `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${fromLang}&tl=${toLang}&dt=t&q=${encodeURIComponent(
-      arrOfLetters.join("\n")
-    )}`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      if (data && data[0] && data[0][0]) {
-        const translatedArray = data[0].map((item) => item[0]);
-        translatedArray.forEach((e, i) => {
-          htext[i].innerText = e;
-        });
-      } else {
-        console.error("Failed to translate the array.");
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-}
+// let englishArray = [];
+// htext.forEach((text) => {
+//   englishArray.push(text.innerText);
+// });
+// function trans(arrOfLetters, fromLang, toLang) {
+//   fetch(
+//     `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${fromLang}&tl=${toLang}&dt=t&q=${encodeURIComponent(
+//       arrOfLetters.join("\n")
+//     )}`
+//   )
+//     .then((response) => response.json())
+//     .then((data) => {
+//       if (data && data[0] && data[0][0]) {
+//         const translatedArray = data[0].map((item) => item[0]);
+//         translatedArray.forEach((e, i) => {
+//           htext[i].innerText = e;
+//         });
+//       } else {
+//         console.error("Failed to translate the array.");
+//       }
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//     });
+// }
 
-if (localStorage.getItem("lang")) {
-  trans(englishArray, "en", "ar");
-}
+// if (localStorage.getItem("lang")) {
+//   trans(englishArray, "en", "ar");
+// }
 
-translate.addEventListener("change", (e) => {
-  if (e.target.value == 3) {
+// translate.addEventListener("change", (e) => {
+//   if (e.target.value == 3) {
    
-    //translate from en to ar
-    trans(englishArray, "en", "ar");
-    localStorage.setItem("lang", "ar");
+//     //translate from en to ar
+//     trans(englishArray, "en", "ar");
+//     localStorage.setItem("lang", "ar");
 
-    ENGLANG2.forEach((e)=>e.style.display='none')
-    ARBLANG2.forEach((e)=>e.style.display='block')
-    Aboutus.style.direction='rtl';
+//     ENGLANG2.forEach((e)=>e.style.display='none')
+//     ARBLANG2.forEach((e)=>e.style.display='block')
+//     Aboutus.style.direction='rtl';
 
-    footer.style.direction='rtl';
-  } else if (e.target.value == 2) {
+//     footer.style.direction='rtl';
+//   } else if (e.target.value == 2) {
  
-    trans(englishArray, "ar", "en");
-    localStorage.removeItem("lang");
+//     trans(englishArray, "ar", "en");
+//     localStorage.removeItem("lang");
 
-    ENGLANG2.forEach((e)=>e.style.display='block')
-    ARBLANG2.forEach((e)=>e.style.display='none')
-    Aboutus.style.direction='ltr';
-    footer.style.direction='ltr';
+//     ENGLANG2.forEach((e)=>e.style.display='block')
+//     ARBLANG2.forEach((e)=>e.style.display='none')
+//     Aboutus.style.direction='ltr';
+//     footer.style.direction='ltr';
 
-  }
-});
+//   }
+// });
+
+
+
+
+
+
+
+
